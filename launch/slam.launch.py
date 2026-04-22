dce03b0e1556b348cd665ee68f6d748fb8f1b5f6
Akhil Shimna Kumar <a.shimnakumar@student.tudelft.nl>
2026-04-22 13:36:36 +0200

    Fix

--- launch/slam.launch.py
+++ launch/slam.launch.py
@@ -1,17 +1,22 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
 
 def generate_launch_description():
 
-    return LaunchDescription([
+    config = os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'config',
+        'slam.yaml'
+    )
 
+    return LaunchDescription([
         Node(
             package='slam_toolbox',
             executable='sync_slam_toolbox_node',
             name='slam_toolbox',
-            parameters=[{
-                'use_sim_time': True
-            }],
+            parameters=[config],
             output='screen'
         )
     ])
