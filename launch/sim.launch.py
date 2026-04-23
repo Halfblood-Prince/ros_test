fa4634561e182928e2f92fa0e4016a1a7f25ad8c
Akhil Shimna Kumar <a.shimnakumar@student.tudelft.nl>
2026-04-23 11:38:13 +0200

    fixed lidar scan

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -12,13 +12,11 @@ def generate_launch_description():
     pkg_share = FindPackageShare('cuboid_robot_description')
     use_rviz = LaunchConfiguration('use_rviz')
     use_slam = LaunchConfiguration('use_slam')
-    use_teleop = LaunchConfiguration('use_teleop')
-    teleop_terminal = LaunchConfiguration('teleop_terminal')
 
     world = PathJoinSubstitution([pkg_share, 'worlds', 'sample_world.sdf'])
     robot_file = PathJoinSubstitution([pkg_share, 'urdf', 'cuboid_robot.urdf.xacro'])
-    slam_params = PathJoinSubstitution([pkg_share, 'config', 'slam_toolbox.yaml'])
     rviz_config = PathJoinSubstitution([pkg_share, 'rviz', 'mapping.rviz'])
+    slam_params = PathJoinSubstitution([pkg_share, 'config', 'slam_toolbox.yaml'])
 
     robot_description = Command([
         FindExecutable(name='xacro'),
@@ -66,19 +64,8 @@ def generate_launch_description():
         executable='parameter_bridge',
         arguments=[
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
-            '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
-            '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry'
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan'
         ],
-        output='screen'
-    )
-
-    teleop = Node(
-        package='teleop_twist_keyboard',
-        executable='teleop_twist_keyboard',
-        prefix=[teleop_terminal, ' '],
-        condition=IfCondition(use_teleop),
-        emulate_tty=True,
         output='screen'
     )
 
@@ -101,13 +88,11 @@ def generate_launch_description():
 
     return LaunchDescription([
         DeclareLaunchArgument('use_rviz', default_value='true'),
-        DeclareLaunchArgument('use_slam', default_value='true'),
-        DeclareLaunchArgument('use_teleop', default_value='false'),
-        DeclareLaunchArgument('teleop_terminal', default_value='xterm -e'),
+        DeclareLaunchArgument('use_slam', default_value='false'),
         gazebo_resource_path,
         gazebo,
         robot_state_publisher,
         TimerAction(period=2.0, actions=[spawn_entity]),
         TimerAction(period=2.5, actions=[bridge]),
-        TimerAction(period=3.0, actions=[teleop, slam, rviz])
+        TimerAction(period=3.0, actions=[slam, rviz])
     ])
